ec56ec374537be25a9db04f63cb5ff7bca544bb9
massinoLight <nacerddine.amassin@gmail.com>
2020-04-18 19:34:44 +0200

    firebase ajoute au projet

---
+++ ListeDesCoursesFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListeDesCoursesFirebase
+//
+//  Created by Massino on 18/04/2020.
+//  Copyright © 2020 Massino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
